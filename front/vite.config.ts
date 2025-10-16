10979ff10dab7eade17c39204bef71b13a551e7b
barma <barmatey@gmail.com>
2025-10-16 14:54:32 +0300

    processing

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,37 +1,45 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import { viteStaticCopy } from "vite-plugin-static-copy"
-import { resolve } from "path"
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+// the below import needs to be added
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import {fileURLToPath} from "node:url";
 
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteStaticCopy({
-      targets: [
-        { src: 'src/extension/manifest.json', dest: '.' },   // копируем manifest.json в dist
-        { src: 'src/extension/popup/popup.html', dest: 'popup' } // копируем popup.html
-      ]
-    })
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, "src/extension/popup/main.ts"),
-        content: resolve(__dirname, "src/extension/content/main.ts")
-      },
-      output: {
-        entryFileNames: (chunk) => {
-          // для popup
-          if (chunk.name === 'popup') return 'popup/main.js'
-          // для контент-скрипта
-          if (chunk.name === 'content') return 'content/main.js'
-          return '[name].js'
+    plugins: [
+        vue(),
+        vueDevTools(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'index.html', // Source path
+                    dest: '' // Destination folder is dist itself
+                }
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         },
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
     },
-    outDir: 'dist',
-    emptyOutDir: true
-  }
+    build: {
+        rollupOptions: {
+            input: {
+                content: 'src/main.js', // Build `main.js` as the content script
+                background: 'src/background/background.js', // Add `background.js` as another entry point
+            },
+            output: {
+                entryFileNames: (chunkInfo) => {
+                    if (chunkInfo.name === 'content') return 'main.js';
+                    if (chunkInfo.name === 'background') return 'background/background.js';
+                    return '[name].js'; // fallback
+                },
+            },
+            external: []
+        },
+    }
 })
