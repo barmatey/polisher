53c09e361da4ca984ec3eb217cf085a823cd6874
barma <barmatey@gmail.com>
2025-10-18 17:14:46 +0300

    processing

--- front/src/content/content.vue
+++ front/src/content/content.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-import SelectedText from "../components/selected-text.vue";
+import {LOGO} from "../logo.ts";
 </script>
 
 <template>
-  <div class="content-wrapper">
-    <selected-text/>
+  <div
+      :style="{
+        'padding': '2rem',
+        'background': '#213547',
+        'color': '#fafafa',
+        'height': '100%',
+        'border-top-left-radius': '1rem',
+        'border-bottom-left-radius': '1rem',
+      }"
+  >
+    <h1>{{ LOGO }}</h1>
   </div>
 </template>
 
 <style scoped>
-.content-wrapper {
-  background: red;
+.temp {
+  background: #213547;
+  border-top-left-radius: 1rem;
 }
 </style>
