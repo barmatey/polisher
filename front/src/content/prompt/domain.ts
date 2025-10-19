6d8512de0f7360aa771d04425f60fc3aaab0aab5
barma <barmatey@gmail.com>
2025-10-19 19:01:07 +0300

    processing

--- front/src/content/prompt/domain.ts
+++ front/src/content/prompt/domain.ts
@@ -1,5 +1,6 @@
 export interface Prompt {
     id: string
+    title: string
     category: "translate" | "improve"
     role: "system" | "user"
     text: string
--- front/src/content/prompt/services.ts
+++ front/src/content/prompt/services.ts
@@ -1,5 +1,4 @@
-import type {Prompt} from "../improve/domain.ts";
-import type {TranslateStrategy} from "../translate/domain.ts";
+import type {Prompt} from "./domain.ts";
 
 export interface PromptService {
     getAllImprovePrompts: () => Promise<Prompt[]>
@@ -10,27 +9,30 @@ export function getPromptService(): PromptService {
     async function getAllImprovePrompts(): Promise<Prompt[]> {
         return [
             {
+                id: '1',
                 title: "Laconic",
-                code: "laconic",
+                category: "improve",
+                role: "system",
+                text: 'Any prompt text',
+                userId: '-1',
+                order: 1,
+                hotkey: 'Ctrl+M',
             },
             {
-                title: "As Churchill",
-                code: "churchill",
-            }
+                id: '2',
+                title: "Churchill",
+                category: "improve",
+                role: "system",
+                text: 'Any prompt text',
+                userId: '-1',
+                order: 2,
+                hotkey: 'Ctrl+O',
+            },
         ]
     }
 
-    async function getAllTranslatePrompts(): Promise<TranslateStrategy[]> {
-        return [
-            {
-                code: "native",
-                title: "Native",
-            },
-            {
-                code: "original",
-                title: "Original",
-            },
-        ]
+    async function getAllTranslatePrompts(): Promise<Prompt[]> {
+        return []
     }
 
     return {getAllImprovePrompts, getAllTranslatePrompts}
