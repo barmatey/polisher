6f320c30b7ff266a8d3727fb68e9d161207aedb4
barma <barmatey@gmai.com>
2025-10-22 17:30:30 +0300

    processing

--- front/src/atoms/simple-menu.vue
+++ front/src/atoms/simple-menu.vue
@@ -2,21 +2,44 @@
 import IconPencil from "../assets/icons/icon-pencil.vue";
 import IconTrash from "../assets/icons/icon-trash.vue";
 import GlassButton from "./glass-button.vue";
-import {ref} from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 
 const e = defineEmits(["edit", "delete"]);
 
-const deleteClicked =ref(false)
-
+const deleteClicked = ref(false);
+const popupRef = ref<HTMLElement | null>(null);
 
 const handleDelete = () => {
   if (!deleteClicked.value) {
-    deleteClicked.value = true
-    return
+    deleteClicked.value = true;
+    return;
   }
-}
+};
 
+const handleClickOutside = (event: MouseEvent) => {
+  if (
+      popupRef.value &&
+      !popupRef.value.contains(event.target as Node)
+  ) {
+    deleteClicked.value = false;
+  }
+};
 
+const handleEscapeKey = (event: KeyboardEvent) => {
+  if (event.key === 'Escape') {
+    deleteClicked.value = false;
+  }
+};
+
+onMounted(() => {
+  document.addEventListener('mousedown', handleClickOutside);
+  document.addEventListener('keydown', handleEscapeKey);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('mousedown', handleClickOutside);
+  document.removeEventListener('keydown', handleEscapeKey);
+});
 </script>
 
 <template>
@@ -40,6 +63,7 @@ const handleDelete = () => {
       />
       <div
           v-if="deleteClicked"
+          ref="popupRef"
           :style="{
             position: 'absolute',
             right: 0,
