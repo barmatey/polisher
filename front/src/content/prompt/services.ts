f5e123a742c8d86c2a7f3033285e8123b644888a
barma <barmatey@gmail.com>
2025-11-01 14:29:49 +0300

    user

--- front/src/content/prompt/services.ts
+++ front/src/content/prompt/services.ts
@@ -2,43 +2,60 @@ import type {Prompt} from "./domain.ts";
 
 export interface PromptService {
     getAllUserPrompts: () => Promise<Prompt[]>
+    updateHotkey: (targetId: string, hotkey: string | null) => Promise<Prompt[]>
 }
 
 export function getPromptService(): PromptService {
+    const prompts = [
+        {
+            id: '1',
+            title: "Laconic",
+            category: "improve",
+            role: "system",
+            text: 'Any prompt text',
+            userId: '-1',
+            order: 1,
+            hotkey: 'Ctrl+M',
+            replacement: {
+                id: "1",
+                code: "inplace",
+                title: "Inplace",
+            }
+        },
+        {
+            id: '2',
+            title: "Churchill",
+            category: "improve",
+            role: "system",
+            text: 'Any prompt text',
+            userId: '-1',
+            order: 2,
+            hotkey: 'Ctrl+O',
+            replacement: {
+                id: "1",
+                code: "inplace",
+                title: "Inplace",
+            }
+        },
+    ]
+
     async function getAllUserPrompts(): Promise<Prompt[]> {
-        return [
-            {
-                id: '1',
-                title: "Laconic",
-                category: "improve",
-                role: "system",
-                text: 'Any prompt text',
-                userId: '-1',
-                order: 1,
-                hotkey: 'Ctrl+M',
-                replacement: {
-                    id: "1",
-                    code: "inplace",
-                    title: "Inplace",
-                }
-            },
-            {
-                id: '2',
-                title: "Churchill",
-                category: "improve",
-                role: "system",
-                text: 'Any prompt text',
-                userId: '-1',
-                order: 2,
-                hotkey: 'Ctrl+O',
-                replacement: {
-                    id: "1",
-                    code: "inplace",
-                    title: "Inplace",
-                }
-            },
-        ]
+        return structuredClone(prompts)
     }
 
-    return {getAllUserPrompts}
+    async function updateHotkey(targetId: string, hotkey: string | null): Promise<Prompt[]> {
+        const result: Prompt[] = []
+        for (let prompt of prompts) {
+            if (prompt.id === targetId) {
+                prompt.hotkey = hotkey
+                result.push(structuredClone(prompt))
+            } else if (prompt.hotkey === hotkey) {
+                prompt.hotkey = null
+                result.push(structuredClone(prompt))
+            }
+        }
+        return result
+    }
+
+    return {getAllUserPrompts, updateHotkey}
 }
